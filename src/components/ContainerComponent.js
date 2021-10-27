d1b59c0460e49bebde8d40305a32752295f4913c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-10-27 08:55:58 -0500

    dropdown selector done

--- src/components/ContainerComponent.js
+++ src/components/ContainerComponent.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Container, Dropdown } from 'react-bootstrap'
 
 import Crypto from './Crypto'
 
@@ -6,8 +7,32 @@ export default function ContainerComponent() {
   const [value, setValue] = useState('bitcoin')
 
   return (
-    <div>
+    <Container>
+      <Dropdown>
+        <Dropdown.Toggle variant="warning" id="dropdown-basic">
+          Select Blockchain Asset
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={() => setValue('bitcoin')}>
+            Bitcoin
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setValue('ethereum')}>
+            Ethereum
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setValue('bitcoin-cash')}>
+            Bitcoin Cash
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setValue('litecoin')}>
+            Litecoin
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setValue('dogecoin')}>
+            Dogecoin
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
       <Crypto asset={value} />
-    </div>
+    </Container>
   )
 }
