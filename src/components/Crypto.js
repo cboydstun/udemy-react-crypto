6ef7ba4294a574bc1cf83603ddad49169eafdf1e
Chris Boydstun <chrisboydstun@gmail.com>
2021-10-27 13:19:49 -0500

    error handling

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,35 +1,41 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import { formatDollars, formatPercentage } from '../utils/format'
+import { formatDollars, formatPercentage } from "../utils/format";
 
 export default function Crypto(props) {
-  const [value, setValue] = useState({})
+  const [value, setValue] = useState({});
 
-  const baseUrl = 'https://api.coincap.io/v2/assets/'
+  const baseUrl = "https://api.coincap.io/v2/assets/";
 
   useEffect(() => {
     axios
       .get(baseUrl + props.asset)
       .then((res) => {
-        setValue(res.data)
-        console.log(res.data)
+        setValue(res.data);
+        console.log(res.data);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [props.asset])
+        console.log(err);
+      });
+  }, [props.asset]);
 
   return (
     <div>
-      <h4>Name: </h4>
-      <p>{value.data.name}</p>
-      <h4>Price: </h4>
-      <p>{formatDollars(value.data.priceUsd)}</p>
-      <h4>Change 24hr: </h4>
-      <p>{formatPercentage(value.data.changePercent24Hr)}%</p>
-      <h4>Symbol: </h4>
-      <p>{value.data.symbol}</p>
+      {value.data ? (
+        <div>
+          <h4>Name: </h4>
+          <p>{value.data.name}</p>
+          <h4>Price: </h4>
+          <p>{formatDollars(value.data.priceUsd)}</p>
+          <h4>Change 24hr: </h4>
+          <p>{formatPercentage(value.data.changePercent24Hr)}%</p>
+          <h4>Symbol: </h4>
+          <p>{value.data.symbol}</p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
-  )
+  );
 }
